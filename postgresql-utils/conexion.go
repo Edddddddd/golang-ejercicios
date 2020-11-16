94aea1949bcfb7bc112cd0fc6e417f38cc63daa7
Eduardo Silva <eduardo.cristobal.silva@gmail.com>
2020-11-16 10:38:54 -0300

    conexion

--- postgresql-utils/conexion.go
+++ postgresql-utils/conexion.go
@@ -8,7 +8,7 @@ import (
 
 // getConnection Obtiene una conexion a la BD
 func GetConnection() *sql.DB {
-	dsn := "postgres://services-gorilas:services-gorilas@104.198.202.57:5432/usuarios?sslmode=disable"
+	dsn := "postgres://[USER]:[PASS]@[IP]:5432/usuarios?sslmode=disable"
 	db, err := sql.Open("postgres", dsn)
 	if err != nil {
 		log.Fatal(err)
